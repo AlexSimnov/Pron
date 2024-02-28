517e5fb0989c231c41268fd7ff6172f3dbc010e2
Симонов Алексей <alxxalxx@mail.ru>
2024-02-28 20:11:56 +0300

    add redis && fix api title

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -8,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2y#rn+=b&47)&l2k3(yevg@b!de-f1fffc(qw*ml3x(dh)y)c1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,6 +32,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'api',
+    'payment',
 ]
 
 MIDDLEWARE = [
@@ -92,16 +96,36 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    # YOUR SETTINGS
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
+API_DESCRIPTION = '''
+Реализовать веб-сервис на базе Django, предоставляющий CRUD REST API для групповых денежных сборов. Приложение должно содержать как минимум три сущности:
+
+1. User (модель Пользователя системы, можно использовать стандартного)
+2. Payment (модель описывающая Платёж для сбора)
+2. Collect (модель непосредственно Группового денежного сбора).
+Групповой сбор должно должен содержать как минимум:
+    - автора сбора
+    - название
+    - повод (один из нескольких на выбор, например, “день рождениеˮ, “свадьбаˮ и т.д.)
+    - описание
+    - сумму, которую запланировали собрать (можно сделать “бесконечныйˮ сбор, вез верхней границы)
+    - сумму, которую собрали на текущий момент сколько человек уже сделало пожертвования обложку сбора (картинка)   - 
+    - дата и время завершения сбора
+    - лента сбора (список пожертвований от других пользователей: сумма, дата и время, ФИО пользователя)
+    - При создании Группового сбора или Платежа по сбору на почту автора/ донатера должно прийти письмо с информацией об успешном создании сбора (отправке платежа).
+    - Все эндпоинты соответствуют REST и покрыты документацией Swagger.
+'''
+
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Your Project API',
-    'DESCRIPTION': 'Your project description',
+    'TITLE': 'CRUD REST API для групповых денежных сборов',
+    'DESCRIPTION': API_DESCRIPTION,
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 LANGUAGE_CODE = 'ru'
 
@@ -114,3 +138,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
